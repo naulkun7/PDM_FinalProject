d0a7acf6b31b5454932c7a29e5d991bf703b7581
naulkun7 <luannguyenn.2k2@gmail.com>
2023-04-24 21:42:10 +0700

    final update

---
+++ src/ConnectMySQL.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ConnectMySQL {
+
+    public void connectDB() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306/HDS?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC", "root", "hoangluan");
+/*
+            System.out.println("Sql:"
+                    + connection.getMetaData().getDatabaseProductName());
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT *FROM login");
+            while (resultSet.next()) {
+                System.out.println("Username:"
+                        + resultSet.getString("UserName"));
+                System.out.println("Password:"
+                        + resultSet.getString("password"));
+            }
+            */
+
+    
+        
+      
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
